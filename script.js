53ddb82c308eb389f7ec645090a276b8c3bf14b6
Gustavo Henrique da Silva <sykogus@gmail.com>
2023-09-02 02:23:13 -0300

    Continuando o código
    Implementando a API do MapQuest, site de API's, que possuem o calculo de uma coordenada latitude/longitude para outra.

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
 // https://brasilapi.com.br/api/cep/v2/{cep}
+// https://www.mapquestapi.com/directions/v2/route?key=YOUR_API_KEY&from=40.7486,-73.9864&to=38.8977,-77.0365
 
-//  var teste = prompt('Digite o CEP')
 
-var url1
-var url2
 
-var coordenadas1
-var coordenadas2
+
+var coordenadas1 = parseInt(prompt('1'));
+var co1Lati, co1Long
+
+var coordenadas2 = parseInt(prompt('2'));
+var co2Lati, co2Long
+
+var resultado
+
 
 // Tratando os dados da API
-var api1 = `https://brasilapi.com.br/api/cep/v2/{89037-030}`;
+const api1 = `https://brasilapi.com.br/api/cep/v2/${coordenadas1}`;
 fetch(api1)
     .then(resposta => resposta.json())
     .then(dados => {
-        coordenadas1 = dados.location.type.point;
-        window.alert(coordenadas1)
+        co1Lati = dados.location.coordinates.latitude;
+        co1Long = dados.location.coordinates.longitude;
     })
 
 
-var api2 = `https://brasilapi.com.br/api/cep/v2/{cep}`;
+
+    
+const api2 = `https://brasilapi.com.br/api/cep/v2/${coordenadas2}`;
 fetch(api2)
     .then(resposta => resposta.json())
     .then(dados => {
-        coordenadas2 = dados.location.type.point;
+        co2Lati = dados.location.coordinates.latitude;
+        co2Long = dados.location.coordinates.longitude;
     })
+
+
+var calculo = `https://www.mapquestapi.com/directions/v2/route?key=FMWbPYju0jXz2kHuWiEEangHsT9aqMoi&from=${co1Lati},${co1Long}&to=${co2Lati},${co2Long}`;
+fetch(calculo)
+    .then(resposta => resposta.json())
+    .then(dados => {
+        resultado = dados.distance;
+        document.write(co1Lati+co1Long)
+    })
+
+
 
